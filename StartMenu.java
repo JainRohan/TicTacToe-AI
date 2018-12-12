2399a6edfff300797d5cb1ef9b0232e5f8ee5221
JainRohan <emailrohanjain@gmail.com>
2018-12-12 13:43:14 -0500

    Create StartMenu.java

---
+++ StartMenu.java
@@ -0,0 +1,19 @@
+/*
+ * Rohan Jain
+ * Grade 12 Computer Science
+ * Tic Tac Toe Artificial Intelligence Application
+ * June 19, 2018
+ */
+
+
+package defaultpac;
+
+import javax.swing.JOptionPane;
+
+public class StartMenu {
+
+	public void Hello() {
+		JOptionPane.showMessageDialog(null, "Hello, Welcome to Rohan's TIC TAC TOE AI. Human plays first as O, followed by the AI as X");
+	}
+	
+}
